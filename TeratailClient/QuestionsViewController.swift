ed9c4688ba1ec3b825763388e87744985e4a6a9a
Shinya Nakajima <nakasho@MacBook-Pro.local>
2018-01-04 23:55:53 +0900

    initial commit

---
+++ TeratailClient/QuestionsViewController.swift
@@ -0,0 +1,14 @@
+import UIKit
+
+class QuestionsViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        self.view.backgroundColor = UIColor.green
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+    }
+}
+
